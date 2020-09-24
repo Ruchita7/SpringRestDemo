1a364d86cff4aa0bc4f60c56d4f8be8900f0c369
Ruchita Maheshwary <ruchita.maheshwary@gmail.com>
2020-09-24 13:33:17 +0530

    spring data rest initial commit

---
+++ springDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(1,'ruchita','java')
+insert into alien values(2,'rachit','management')
+insert into alien  values(3,'rohit','salesforce')
+insert into alien   values(4,'priyanka','java')
+insert into alien  values(5,'komal','javascript')
